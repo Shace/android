1c73ee2861fc14d9acc070556332dd9309b8a61e
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2014-08-09 13:18:55 -0700

    Fix imeAction

--- app/src/main/java/io/shace/app/SignUpActivity.java
+++ app/src/main/java/io/shace/app/SignUpActivity.java
@@ -1,11 +1,15 @@
 package io.shace.app;
 
 import android.app.Activity;
+import android.view.KeyEvent;
 import android.view.Window;
+import android.view.inputmethod.EditorInfo;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
@@ -19,13 +23,19 @@ import io.shace.app.tools.ToastTools;
         Window.FEATURE_INDETERMINATE_PROGRESS
 })
 @EActivity(R.layout.activity_sign_up)
-public class SignUpActivity extends Activity {
+public class SignUpActivity extends Activity implements TextView.OnEditorActionListener {
     @ViewById(R.id.icon_loader) protected ProgressBar mIconLoader;
 
     @ViewById(R.id.first_name) protected AutoCompleteTextView mFirstNameView;
     @ViewById(R.id.last_name) protected AutoCompleteTextView mLastNameView;
     @ViewById(R.id.email) protected AutoCompleteTextView mEmailView;
     @ViewById(R.id.password) protected EditText mPasswordView;
+
+
+    @AfterViews
+    protected void init() {
+        mPasswordView.setOnEditorActionListener(this);
+    }
 
     @Click(R.id.signUpButton)
     protected void signUp() {
@@ -37,5 +47,13 @@ public class SignUpActivity extends Activity {
         User user = new User(email, password, firstName, lastName, null);
         //user.save(this);
         ToastTools.use().longToast("OK");
+    }
+
+    @Override
+    public boolean onEditorAction(TextView textView, int actionID, KeyEvent keyEvent) {
+        if (actionID == EditorInfo.IME_ACTION_DONE) {
+            signUp();
+        }
+        return true;
     }
 }
