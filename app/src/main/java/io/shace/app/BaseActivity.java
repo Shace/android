0c5c98acdcec963d25a630e9e83b1e6e169979b9
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2014-08-11 00:19:58 -0700

    Add a method to handle form errors

--- app/src/main/java/io/shace/app/BaseActivity.java
+++ app/src/main/java/io/shace/app/BaseActivity.java
@@ -2,22 +2,33 @@ package io.shace.app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.util.Map;
+
+import io.shace.app.api.ApiError;
 
 /**
  * Created by melvin on 8/10/14.
  */
 public class BaseActivity extends Activity {
+    private static final String TAG = BaseActivity.class.getSimpleName();
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
+
     protected void onResume() {
         super.onResume();
         App.setCurrentActivity(this);
     }
+
     protected void onPause() {
         clearReferences();
         super.onPause();
     }
+
     protected void onDestroy() {
         clearReferences();
         super.onDestroy();
@@ -27,5 +38,32 @@ public class BaseActivity extends Activity {
         Activity currActivity = App.getCurrentActivity();
         if (currActivity != null && currActivity.equals(this))
             App.setCurrentActivity(null);
+    }
+
+    protected boolean checkFormError(ApiError error, Map<String, TextView> fields) {
+        Map<String,String> params = error.getParameters();
+
+        if (params.size() > 0) {
+            for (Map.Entry<String, String> entry : params.entrySet()) {
+                String field = entry.getKey();
+                String type = entry.getValue();
+
+                TextView view = fields.get(field);
+                if (view != null) {
+                    String message = ApiError.getErrorMessage(type);
+
+                    if (message != null) {
+                        view.setError(message);
+                    }
+                    else {
+                        view.setError("Unknown error");
+                    }
+                } else {
+                    Log.e(TAG, "FormValidation error: Unknown field '" + field + "'");
+                }
+            }
+            return true;
+        }
+        return false;
     }
 }
