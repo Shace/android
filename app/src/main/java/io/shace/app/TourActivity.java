f0e858a5a91e6e07d3f1b8396fdd9062c40be0fe
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2014-08-11 20:37:47 -0700

    Clean TourActivity

--- app/src/main/java/io/shace/app/TourActivity.java
+++ app/src/main/java/io/shace/app/TourActivity.java
@@ -1,17 +1,11 @@
 package io.shace.app;
 
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
-
-import io.shace.app.R;
 
 public class TourActivity extends Activity {
 
@@ -24,26 +18,6 @@ public class TourActivity extends Activity {
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.tour, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
     /**
