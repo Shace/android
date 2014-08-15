68ab74ab629f324266c7385f76a9a50c2418ca19
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2014-08-15 01:03:13 -0700

    Add getters and setters

--- app/src/main/java/io/shace/app/api/models/Event.java
+++ app/src/main/java/io/shace/app/api/models/Event.java
@@ -25,6 +25,70 @@ public class Event extends Model {
     // private User owner;
 
 
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getReadingPrivacy() {
+        return readingPrivacy;
+    }
+
+    public void setReadingPrivacy(String readingPrivacy) {
+        this.readingPrivacy = readingPrivacy;
+    }
+
+    public String getWritingPrivacy() {
+        return writingPrivacy;
+    }
+
+    public void setWritingPrivacy(String writingPrivacy) {
+        this.writingPrivacy = writingPrivacy;
+    }
+
+    public String getReadingPassword() {
+        return readingPassword;
+    }
+
+    public void setReadingPassword(String readingPassword) {
+        this.readingPassword = readingPassword;
+    }
+
+    public String getWritingPassword() {
+        return writingPassword;
+    }
+
+    public void setWritingPassword(String writingPassword) {
+        this.writingPassword = writingPassword;
+    }
+
+    public String getCreation() {
+        return creation;
+    }
+
+    public void setCreation(String creation) {
+        this.creation = creation;
+    }
+
     public static void search(EventListener listener, String query) {
         Map<String, String> data = new HashMap<String, String>();
         data.put("query", query);
