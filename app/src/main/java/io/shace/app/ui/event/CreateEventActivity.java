0305a51a73213214b79a68d522d2d6262b01189c
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2014-08-23 20:31:57 -0700

    Get the token and disable the token field if there is a token

--- app/src/main/java/io/shace/app/ui/event/CreateEventActivity.java
+++ app/src/main/java/io/shace/app/ui/event/CreateEventActivity.java
@@ -1,12 +1,32 @@
 package io.shace.app.ui.event;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.EditText;
 
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
 
 import io.shace.app.R;
+import io.shace.app.api.filters.TokenFilter;
 
 @EActivity(R.layout.activity_create_event)
 public class CreateEventActivity extends Activity {
+    @ViewById(R.id.token) protected EditText mToken;
 
+    @AfterViews
+    protected void init() {
+        mToken.setFilters(new TokenFilter[]{ new TokenFilter() });
+
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+
+        if (bundle != null){
+            String token = bundle.getString(Intent.EXTRA_TEXT);
+            mToken.setText(token);
+            mToken.setEnabled(false);
+        }
+    }
 }
